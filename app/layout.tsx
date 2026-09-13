ec01862c61e958fa46e0e1ad1cb31b3dd54ac243
gami-official <k_fuchigami@gamigami.email>
2026-09-13 20:42:43 +0900

    Integrate Google Tag Manager into layout
    Added Google Tag Manager script and noscript tags to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import type { Metadata, Viewport } from "next"
 import { Noto_Sans_JP, Noto_Serif_JP } from "next/font/google"
-import { GoogleTagManager } from "@next/third-parties/google"
 
 import "./globals.css"
 
@@ -113,13 +112,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TJWG5GT5');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+
       <body
         className={`${notoSansJP.variable} ${notoSerifJP.variable} bg-[#fbfaf7] font-sans text-[#111111] antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJWG5GT5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
       </body>
-
-      <GoogleTagManager gtmId="GTM-TJWG5GT5" />
     </html>
   )
 }
